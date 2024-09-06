0386a393bd5c7969df6c02f8436cf505dbf39971
vergaraCoder <jhonatanvergara072@gmail.com>
2024-09-05 21:52:59 -0400

    add file main.ts

---
+++ app/backEnd/api-gateway/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { ApiGatewayModule } from './api-gateway.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(ApiGatewayModule);
+  app.enableCors();
+  
+  await app.listen(3000);
+}
+bootstrap();
