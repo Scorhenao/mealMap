eeaaf0c61ca716a8a364aa6adb82b87418720173
vergaraCoder <jhonatanvergara072@gmail.com>
2024-09-12 21:31:22 -0500

    creation of the webSOcket standar

--- app/backEnd/api-gateway/src/web-socket-gateway/web-socket-gateway.gateway.ts
+++ app/backEnd/api-gateway/src/web-socket-gateway/web-socket-gateway.gateway.ts
@@ -1,22 +1,63 @@
 import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer, ConnectedSocket } from '@nestjs/websockets';
 import { WebSocketGatewayService } from './web-socket-gateway.service';
-import { Server, Socket } from 'socket.io';
+import {WebSocket} from 'ws';
+
+import { OnModuleInit } from '@nestjs/common';
+import { rejects } from 'assert';
 
 @WebSocketGateway({
   cors: {
     origin: '*',
   },
 })
-export class WebSocketGatewayGateway {
+export class WebSocketGatewayGateway implements OnModuleInit{
   constructor(private readonly webSocketGatewayService: WebSocketGatewayService) {}
 
-  @WebSocketServer()
-  server:Server;
+  private client:WebSocket;
+  private pendingResponses: Map<number, (response: any) => void> = new Map();
+
+  onModuleInit() {
+      this.client=new WebSocket("ws://localhost:8080/ws/myHandler");
+
+      this.client.on("open",()=>{
+        //console.log("Connected to server");
+      });
+
+      this.client.on("message",(data)=>{
+        const message = JSON.parse(data.toString());
+        //console.log(message);
+        
+
+        if(message.id && this.pendingResponses.has(message.id)){
+          const resolve=this.pendingResponses.get(message.id);
+          resolve(message.response);
+          this.pendingResponses.delete(message.id);
+        }
+
+      });
+
+      this.client.on("error",()=>{
+       // console.log("Connected to server");
+      });
+
+      // this.client.on("close",()=>{
+      //   console.log("Connected to server");
+      // });
+
+  }
+
 
   @SubscribeMessage('message')
-  create(@ConnectedSocket() client:Socket,@MessageBody() createWebSocketGatewayDto: any) {
-    //this.server.emit('ola',createWebSocketGatewayDto);
-    client.broadcast.emit("ola",createWebSocketGatewayDto);
+  create(@ConnectedSocket() client:WebSocket,@MessageBody() message: any) {
+
+    if (this.client.readyState === WebSocket.OPEN) {
+
+     this.client.send(JSON.stringify(message));
+      
+      return new Promise((resolve,rejects)=>{
+        
+      })
+    }
   }
 
   @SubscribeMessage('findAllWebSocketGateway')
