8a7862c2c4c0c498c563890f6e894abfe3c7a413
vergaraCoder <jhonatanvergara072@gmail.com>
2024-09-19 16:26:26 -0500

    delete files

--- app/backEnd/api-gateway/dist/caching/caching.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-import { Cache } from '@nestjs/cache-manager';
-export declare class CachingService {
-    private cache;
-    private httpService;
-    constructor(cache: Cache, httpService: HttpService);
-    getCachedData(key: string): Promise<unknown>;
-}
--- app/backEnd/api-gateway/dist/web-socket-gateway/web-socket-gateway.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class WebSocketGatewayService {
-    create(createWebSocketGatewayDto: any): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateWebSocketGatewayDto: any): string;
-    remove(id: number): string;
-}
